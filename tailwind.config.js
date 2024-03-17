ce79eeb0f443e04c26e868b40fc9c0f03624e3e3
AryanSMD <aryansmd79@gmail.com>
2024-03-18 00:28:07 +0330

    about and  skills created

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "*./src/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#0E1118',
+        secondary: '#253F6F',
+        tertiary: 'red',
+      }
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+    }
   },
   plugins: [],
 }
