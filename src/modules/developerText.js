7af90e5104e84907b6d09658cde2671e6ed88151
AryanSMD <aryansmd79@gmail.com>
2024-10-01 18:45:40 +0330

    updated

--- src/modules/developerText.js
+++ src/modules/developerText.js
@@ -1,6 +1,10 @@
 const developer = document.getElementById('developer');
 
 export default function setDeveloperStyle(scroll) {
-    $('#developer').css('opacity', `${ (100 - scroll * .4) >= 0 ? 100 - scroll * .4 : 0 }%`)
-    $('#developer').css('transform', `scale(${ (1 - scroll * .002) >= 0 ? 1 - scroll * .002 : 0 })`)
+    if (window.screen.width > 1024) {
+        $('#developer').css('opacity', `${ (100 - scroll * .4) >= 0 ? 100 - scroll * .4 : 0 }%`)
+        $('#developer').css('transform', `scale(${ (1 - scroll * .002) >= 0 ? 1 - scroll * .002 : 0 })`)
+    } else {
+        $('#developer').css('transform', `translateX(-${scroll * 8 < 1000 ? scroll * 8 : 1000}px)`);
+    }
 }
--- src/modules/frontendText.js
+++ src/modules/frontendText.js
@@ -1,5 +1,9 @@
 const frontend = document.getElementById('frontend');
 
 export default function setFrontendStyle(scroll) {
-    $('#frontend').css('transform', `translateX(${scroll * 4 < 1000 ? scroll * 4 : 1000}px)`)
+    if (window.screen.width > 1024) {
+        $('#frontend').css('transform', `translateX(${scroll * 4 < 1000 ? scroll * 4 : 1000}px)`);
+    } else {
+        $('#frontend').css('transform', `translateX(${scroll * 8 < 1000 ? scroll * 8 : 1000}px)`);
+    }
 }
