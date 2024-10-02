61d3434e3873110c309438d4de1e80130aaa01cd
AryanSMD <aryansmd79@gmail.com>
2024-10-02 18:11:14 +0330

    updated

--- src/main.js
+++ src/main.js
@@ -2,18 +2,18 @@ AOS.init();
 import setFrontendStyle from './modules/frontendText.js';
 import setDeveloperStyle from './modules/developerText.js';
 
-
 const home = document.getElementById('homeLink');
 const about = document.getElementById('aboutLink');
 const education = document.getElementById('educationLink');
 const skills = document.getElementById('skillsLink');
 const experience = document.getElementById('experienceLink');
-
+const contactBtn1 = document.getElementById('contactBtn1');
+const contactBtn2 = document.getElementById('contactBtn2');
+const closeModalBtn = document.getElementById('close');
 
 function setColor(elem) {
     elem.classList.add('!text-primary');
 }
-
 function clear() {
     home.classList.remove('!text-primary');
     about.classList.remove('!text-primary');
@@ -21,14 +21,7 @@ function clear() {
     skills.classList.remove('!text-primary');
     experience.classList.remove('!text-primary');
 }
-
-setColor(home)
-window.addEventListener('scroll', () => {
-    const scroll = window.scrollY;
-
-    setFrontendStyle(scroll);
-    setDeveloperStyle(scroll);
-
+function setMenuStyle(scroll) {
     if (scroll < window.screen.height - 500) {
         clear();
         setColor(home);
@@ -45,4 +38,23 @@ window.addEventListener('scroll', () => {
         clear();
         setColor(experience);
     }
+}
+
+contactBtn1.addEventListener('click', () => {
+    clear();
 })
+contactBtn2.addEventListener('click', () => {
+    clear();
+})
+closeModalBtn.addEventListener('click', () => {
+    setMenuStyle(window.scrollY);
+})
+
+window.addEventListener('scroll', () => {
+    const scroll = window.scrollY;
+    setFrontendStyle(scroll);
+    setDeveloperStyle(scroll);
+    setMenuStyle(scroll);
+})
+
+setMenuStyle(window.scrollY)
